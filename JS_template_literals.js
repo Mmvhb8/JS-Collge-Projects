6f240522d65f3011d15821037214c70e02b4fd1e
Bryan Hamm <bryanhamm02@gmail.com>
2026-09-11 12:00:25 -0400

    Learning JS's f string equivalent

---
+++ JS_template_literals.js
@@ -0,0 +1,11 @@
+let hero = "Gensk"
+let level = 6
+let gold = 500
+let cls = "Fighter"
+
+console.log(`The hero ${hero} is a level ${level} ${cls} and has ${gold} gold.`)
+console.log(`
+    ${hero}: ${cls}
+    Level: ${level}
+    Gold: ${gold}
+`);
